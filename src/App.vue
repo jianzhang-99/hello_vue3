7d4b70eb83822ee489037ede4e99852c9c0d866f
liangjiajian <653895012@qq.com>
2024-02-04 14:44:01 +0800

    自己写一个vue组建

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="app">
+    <h1>Hello, Vue 3!</h1>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts">
+  export default {
+    name: 'App',
+  };
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style >
+  .app { 
+    background-color:#ddd ;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    padding: 20px;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import './assets/main.css'
 
+// 引入vue库
 import { createApp } from 'vue'
+// 引入App.vue组件
 import App from './App.vue'
 
+// 创建vue实例
 createApp(App).mount('#app')
